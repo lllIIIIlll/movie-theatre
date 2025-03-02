57baa81d930c9011cf03f5cf22994cf8296ec31e
Oliver <hohang210@gmail.com>
2025-03-02 16:38:22 +0800

    [FEAT] Configure Request Information Request Logging Filter

--- src/main/java/net/ow/movie/theatre/config/LoggingConfig.java
+++ src/main/java/net/ow/movie/theatre/config/LoggingConfig.java
@@ -1,7 +1,12 @@
 package net.ow.movie.theatre.config;
 
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Getter;
+import net.ow.shared.commonlog.filter.RequestInformationRequestLoggingFilter;
 import net.ow.shared.commonlog.filter.TraceIdRequestLoggingFilter;
+import net.ow.shared.commonlog.provider.client.information.ClientInformationProvider;
+import net.ow.shared.commonlog.provider.client.information.JWTClientInformationProvider;
 import net.ow.shared.commonlog.provider.trace.HeaderBasedTraceIdProvider;
 import net.ow.shared.commonlog.provider.trace.TraceIdProvider;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +16,11 @@ import org.springframework.context.annotation.Configuration;
 @Getter
 @Configuration
 public class LoggingConfig {
-    @Value("${logging.trace-id.header}")
+    @Value("${movie-theatre.trace-id.header}")
     private String traceIdHeader;
+
+    @Value("${movie-theatre.user-id.jwt-claim}")
+    private String userIdJwtClaim;
 
     @Bean
     public TraceIdRequestLoggingFilter traceIdRequestLoggingFilter() {
@@ -22,5 +30,24 @@ public class LoggingConfig {
         traceIdRequestLoggingFilter.setTraceIdProvider(traceIdProvider);
 
         return traceIdRequestLoggingFilter;
+    }
+
+    @Bean
+    public RequestInformationRequestLoggingFilter requestInformationRequestLoggingFilter() {
+        RequestInformationRequestLoggingFilter requestInformationRequestLoggingFilter =
+                new RequestInformationRequestLoggingFilter();
+
+        Set<String> clientInformationJwtClaims = new HashSet<>();
+        clientInformationJwtClaims.add(userIdJwtClaim);
+        ClientInformationProvider clientInformationProvider =
+                new JWTClientInformationProvider(clientInformationJwtClaims);
+
+        requestInformationRequestLoggingFilter.setClientInformationProvider(
+                clientInformationProvider);
+        requestInformationRequestLoggingFilter.setIncludeClientInfo(true);
+        requestInformationRequestLoggingFilter.setIncludeQueryString(true);
+        requestInformationRequestLoggingFilter.setIncludePayload(true);
+
+        return requestInformationRequestLoggingFilter;
     }
 }
--- src/test/java/net/ow/movie/theatre/config/LoggingConfigTest.java
+++ src/test/java/net/ow/movie/theatre/config/LoggingConfigTest.java
@@ -2,7 +2,10 @@ package net.ow.movie.theatre.config;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import net.ow.shared.commonlog.filter.RequestInformationRequestLoggingFilter;
 import net.ow.shared.commonlog.filter.TraceIdRequestLoggingFilter;
+import net.ow.shared.commonlog.provider.client.information.ClientInformationProvider;
+import net.ow.shared.commonlog.provider.client.information.JWTClientInformationProvider;
 import net.ow.shared.commonlog.provider.trace.HeaderBasedTraceIdProvider;
 import net.ow.shared.commonlog.provider.trace.TraceIdProvider;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,19 +15,23 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+@SuppressWarnings("all")
 @ExtendWith(MockitoExtension.class)
 class LoggingConfigTest {
     @InjectMocks private LoggingConfig loggingConfig;
 
     private final String traceIdHeader = "Trace-Id";
 
+    private final String userIdJwtClaim = "userId";
+
     @BeforeEach
     void setUp() {
         ReflectionTestUtils.setField(loggingConfig, "traceIdHeader", traceIdHeader);
+        ReflectionTestUtils.setField(loggingConfig, "userIdJwtClaim", userIdJwtClaim);
     }
 
     @Test
-    public void traceIdRequestLoggingFilter_ShouldReturnConfiguredFilter() {
+    void traceIdRequestLoggingFilterTest_OK() {
         TraceIdRequestLoggingFilter filter = loggingConfig.traceIdRequestLoggingFilter();
         assertInstanceOf(
                 TraceIdRequestLoggingFilter.class,
@@ -44,5 +51,33 @@ class LoggingConfigTest {
                 traceIdHeader,
                 ReflectionTestUtils.getField(headerBasedTraceIdProvider, "traceIdHeader"),
                 "Trace ID header should match the configured value");
+    }
+
+    @Test
+    void requestInformationRequestLoggingFilterTest_OK() {
+        RequestInformationRequestLoggingFilter filter =
+                loggingConfig.requestInformationRequestLoggingFilter();
+        assertInstanceOf(
+                RequestInformationRequestLoggingFilter.class,
+                filter,
+                "Expected an instance of RequestInformationRequestLoggingFilter");
+
+        ClientInformationProvider clientInformationProvider =
+                (ClientInformationProvider)
+                        ReflectionTestUtils.getField(filter, "clientInformationProvider");
+        assertInstanceOf(
+                JWTClientInformationProvider.class,
+                clientInformationProvider,
+                "Expected an instance of JWTClientInformationProvider");
+
+        assertTrue(
+                (boolean) ReflectionTestUtils.getField(filter, "includeClientInfo"),
+                "includeClientInfo should be true");
+        assertTrue(
+                (boolean) ReflectionTestUtils.getField(filter, "includeQueryString"),
+                "includeQueryString should be true");
+        assertTrue(
+                (boolean) ReflectionTestUtils.getField(filter, "includePayload"),
+                "includePayload should be true");
     }
 }
