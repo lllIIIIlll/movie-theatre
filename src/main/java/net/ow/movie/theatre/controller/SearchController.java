6373f9d22e60d22470fdf2df4aba407f8e81797e
hohang210 <35669859+hohang210@users.noreply.github.com>
2025-03-17 17:07:18 +0800

    [FEAT] Expose an endpoint for searching movies (#11)
    ## Description
    
    - Expose an endpoint for searching movies
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] This PR does not introduce a breaking change
    - [x] Unit tests are added/updated (if applicable)
    - [x] No error nor warning in the console
    - [ ] I have uploaded a screenshot (if applicable)

---
+++ src/main/java/net/ow/movie/theatre/controller/SearchController.java
@@ -0,0 +1,22 @@
+package net.ow.movie.theatre.controller;
+
+import lombok.RequiredArgsConstructor;
+import net.ow.movie.theatre.dto.pagination.PaginatedResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SearchController {
+    @GetMapping("/search")
+    public ResponseEntity<PaginatedResponse> search(
+            @RequestParam String query,
+            @RequestParam(required = false, defaultValue = "1") Integer pageNumber,
+            @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE) String language) {
+        throw new UnsupportedOperationException();
+    }
+}
