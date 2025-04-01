dfa4f7282d8d16952005762a66fe6f73623a26e3
hohang210 <35669859+hohang210@users.noreply.github.com>
2025-04-01 17:55:05 +0800

    [TEST] Update unit test for search service to use mock data from fixture (#34)
    ## Description
    
    - Update unit test for search service to use mock data from fixture
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] This PR does not introduce a breaking change
    - [x] Unit tests are added/updated (if applicable)
    - [x] No error nor warning in the console
    - [ ] I have uploaded a screenshot (if applicable)

--- src/test/java/net/ow/movie/theatre/fixture/MockPaginatedResponse.java
+++ src/test/java/net/ow/movie/theatre/fixture/MockPaginatedResponse.java
@@ -3,10 +3,23 @@ package net.ow.movie.theatre.fixture;
 import java.util.List;
 import net.ow.movie.theatre.dto.movie.BaseMovieDTO;
 import net.ow.movie.theatre.dto.pagination.PaginatedResponse;
+import net.ow.movie.theatre.dto.search.SearchResultDTO;
 
 public class MockPaginatedResponse {
     public static PaginatedResponse<BaseMovieDTO> mockPaginatedBaseMovie(List<BaseMovieDTO> data) {
         PaginatedResponse<BaseMovieDTO> paginatedResponse = new PaginatedResponse<>();
+
+        paginatedResponse.setData(data);
+        paginatedResponse.setPage(1);
+        paginatedResponse.setTotalPages(1);
+        paginatedResponse.setTotal(null == data ? 0 : data.size());
+
+        return paginatedResponse;
+    }
+
+    public static PaginatedResponse<SearchResultDTO> mockPaginatedSearchResult(
+            List<SearchResultDTO> data) {
+        PaginatedResponse<SearchResultDTO> paginatedResponse = new PaginatedResponse<>();
 
         paginatedResponse.setData(data);
         paginatedResponse.setPage(1);
