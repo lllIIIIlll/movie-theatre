bf6c95b041d162c8d880127b8fed41c111fbe61d
hohang210 <35669859+hohang210@users.noreply.github.com>
2025-04-01 17:35:01 +0800

    [FEAT] Implement an API to fetch movie's genres (#32)
    ## Description
    
    - Implement an API to fetch movie's genres
    
    ## Screenshot / Video
    ![截屏2025-03-31 20 18
    46](https://github.com/user-attachments/assets/e59d9aa8-ab9f-4d39-8e3b-49a0d4a38170)
    
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] This PR does not introduce a breaking change
    - [x] Unit tests are added/updated (if applicable)
    - [x] No error nor warning in the console
    - [x] I have uploaded a screenshot (if applicable)

--- src/test/java/net/ow/movie/theatre/service/GenreServiceTest.java
+++ src/test/java/net/ow/movie/theatre/service/GenreServiceTest.java
@@ -3,10 +3,12 @@ package net.ow.movie.theatre.service;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import feign.FeignException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import net.ow.movie.theatre.dto.genre.GenreDTO;
+import net.ow.movie.theatre.fixture.MockGenreDTO;
 import net.ow.movie.theatre.mapper.genre.GenreDTOMapper;
 import net.ow.movie.tmdb.feign.TMDBFeignClient;
 import net.ow.movie.tmdb.model.genre.TMDBGenreList;
@@ -100,5 +102,50 @@ class GenreServiceTest {
         assertEquals(genre2, genreMap.get(1));
         verify(tmdbFeignClient, times(1)).getGenres(language);
         verify(genreDTOMapper, times(1)).fromTMDBGenreList(tmdbGenreList);
+    }
+
+    @Test
+    void getMovieGenresTest_OK() {
+        String language = "zh-CN";
+
+        GenreDTO genre1 = MockGenreDTO.mock(1, "genre-name");
+        GenreDTO genre2 = MockGenreDTO.mock(2, "genre-name");
+
+        List<GenreDTO> genres = List.of(genre1, genre2);
+
+        when(tmdbFeignClient.getMovieGenres(language)).thenReturn(tmdbGenreList);
+        when(genreDTOMapper.fromTMDBGenreList(tmdbGenreList)).thenReturn(genres);
+
+        List<GenreDTO> actualGenres = genreService.getMovieGenres(language);
+
+        assertEquals(genres, actualGenres);
+        verify(tmdbFeignClient, times(1)).getMovieGenres(language);
+        verify(genreDTOMapper, times(1)).fromTMDBGenreList(tmdbGenreList);
+    }
+
+    @Test
+    void getMovieGenresTest_whenEmptyResponse_thenReturnsEmptyList() {
+        String language = "zh-CN";
+
+        when(tmdbFeignClient.getMovieGenres(language)).thenReturn(tmdbGenreList);
+        when(genreDTOMapper.fromTMDBGenreList(tmdbGenreList)).thenReturn(Collections.emptyList());
+
+        List<GenreDTO> actualGenres = genreService.getMovieGenres(language);
+
+        assertTrue(actualGenres.isEmpty());
+        verify(tmdbFeignClient, times(1)).getMovieGenres(language);
+        verify(genreDTOMapper, times(1)).fromTMDBGenreList(tmdbGenreList);
+    }
+
+    @Test
+    void getMovieGenresTest_whenThrowFeignExceptionException_thenThrowsException() {
+        String language = "zh-CN";
+
+        when(tmdbFeignClient.getMovieGenres(language)).thenThrow(FeignException.class);
+
+        assertThrows(FeignException.class, () -> genreService.getMovieGenres(language));
+
+        verify(tmdbFeignClient, times(1)).getMovieGenres(language);
+        verify(genreDTOMapper, never()).fromTMDBGenreList(any());
     }
 }
