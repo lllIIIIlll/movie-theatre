f1643af10f6e55525ffe8943dd6b0d7ef6c60b8f
Oliver <hohang210@gmail.com>
2025-04-05 13:38:37 +0800

    [FEAT] Add a validation for page request parameter

--- src/main/java/net/ow/movie/theatre/controller/MovieController.java
+++ src/main/java/net/ow/movie/theatre/controller/MovieController.java
@@ -1,5 +1,8 @@
 package net.ow.movie.theatre.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import net.ow.movie.theatre.dto.movie.BaseMovieDTO;
 import net.ow.movie.theatre.dto.pagination.PaginatedResponse;
@@ -7,8 +10,10 @@ import net.ow.movie.theatre.dto.trending.TrendingMovieDTO;
 import net.ow.movie.theatre.service.MovieService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @CrossOrigin
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +24,11 @@ public class MovieController {
     @GetMapping("/popular")
     public ResponseEntity<PaginatedResponse<BaseMovieDTO>> getPopularMovies(
             @RequestParam(required = false) String region,
-            @RequestParam(required = false, defaultValue = "1") Integer page,
+            @RequestParam(required = false, defaultValue = "1")
+                    @Valid
+                    @Min(value = 1, message = "Page must be at least 1")
+                    @Max(value = 500, message = "Page must be less then 500")
+                    Integer page,
             @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE) String language) {
         return ResponseEntity.ok(movieService.getPopularMovies(language, page, region));
     }
@@ -27,7 +36,11 @@ public class MovieController {
     @GetMapping("/now-playing")
     public ResponseEntity<PaginatedResponse<BaseMovieDTO>> getNowPlayingMovies(
             @RequestParam(required = false) String region,
-            @RequestParam(required = false, defaultValue = "1") Integer page,
+            @RequestParam(required = false, defaultValue = "1")
+                    @Valid
+                    @Min(value = 1, message = "Page must be at least 1")
+                    @Max(value = 500, message = "Page must be less then 500")
+                    Integer page,
             @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE) String language) {
         return ResponseEntity.ok(movieService.getNowPlayingMovies(language, page, region));
     }
@@ -35,7 +48,11 @@ public class MovieController {
     @GetMapping("/trending/{time_window}")
     public ResponseEntity<PaginatedResponse<TrendingMovieDTO>> getTrendingMovies(
             @PathVariable(value = "time_window") String timeWindow,
-            @RequestParam(required = false, defaultValue = "1") Integer page,
+            @RequestParam(required = false, defaultValue = "1")
+                    @Valid
+                    @Min(value = 1, message = "Page must be at least 1")
+                    @Max(value = 500, message = "Page must be less then 500")
+                    Integer page,
             @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE) String language) {
         return ResponseEntity.ok(movieService.getTrendingMovies(timeWindow, language, page));
     }
