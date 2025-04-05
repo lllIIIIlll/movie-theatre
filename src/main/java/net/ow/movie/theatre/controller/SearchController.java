784079777cda8f5b27bcbedf9a0cf3dce8392038
hohang210 <35669859+hohang210@users.noreply.github.com>
2025-04-05 13:40:29 +0800

    [FEAT] Implement validation for page request parameter (#45)
    ## Description
    
    - Implement validation for page request parameter
    
    ## Screenshot / Video
    ![截屏2025-04-05 13 37
    50](https://github.com/user-attachments/assets/e2011751-e597-4572-8cb8-b306ece75b8a)
    
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] This PR does not introduce a breaking change
    - [ ] Unit tests are added/updated (if applicable)
    - [x] No error nor warning in the console
    - [x] I have uploaded a screenshot (if applicable)

--- src/main/java/net/ow/movie/theatre/controller/SearchController.java
+++ src/main/java/net/ow/movie/theatre/controller/SearchController.java
@@ -1,13 +1,18 @@
 package net.ow.movie.theatre.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import net.ow.movie.theatre.dto.pagination.PaginatedResponse;
 import net.ow.movie.theatre.dto.search.SearchResultDTO;
 import net.ow.movie.theatre.service.SearchService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @CrossOrigin
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +22,11 @@ public class SearchController {
     @GetMapping("/search")
     public ResponseEntity<PaginatedResponse<? extends SearchResultDTO>> search(
             @RequestParam String query,
-            @RequestParam(required = false, defaultValue = "1") Integer pageNumber,
+            @RequestParam(required = false, defaultValue = "1")
+                    @Valid
+                    @Min(value = 1, message = "Page must be at least 1")
+                    @Max(value = 500, message = "Page must be less then 500")
+                    Integer pageNumber,
             @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE) String language) {
         return ResponseEntity.ok(searchService.search(query, pageNumber, language));
     }
--- src/main/java/net/ow/movie/theatre/controller/TVShowController.java
+++ src/main/java/net/ow/movie/theatre/controller/TVShowController.java
@@ -1,13 +1,18 @@
 package net.ow.movie.theatre.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import net.ow.movie.theatre.dto.pagination.PaginatedResponse;
 import net.ow.movie.theatre.dto.trending.TrendingTVShowDTO;
 import net.ow.movie.theatre.service.TVShowService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @CrossOrigin
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +23,11 @@ public class TVShowController {
     @GetMapping("/trending/{time_window}")
     public ResponseEntity<PaginatedResponse<TrendingTVShowDTO>> getTrendingTVShows(
             @PathVariable(value = "time_window") String timeWindow,
-            @RequestParam(required = false, defaultValue = "1") Integer page,
+            @RequestParam(required = false, defaultValue = "1")
+                    @Valid
+                    @Min(value = 1, message = "Page must be at least 1")
+                    @Max(value = 500, message = "Page must be less then 500")
+                    Integer page,
             @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE) String language) {
         return ResponseEntity.ok(tvShowService.getTrendingTVShows(timeWindow, page, language));
     }
