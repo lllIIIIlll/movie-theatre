4362fb04db799dee2264b3dcbaa3d2e684572333
hohang210 <35669859+hohang210@users.noreply.github.com>
2025-06-15 12:03:57 +0300

    [RFT] Refactor the endpoint path of genres (#76)
    ## Description
    
    - Refactor the endpoint path of genres
    - Update swagger API documentation
    
    ## Screenshot / Video
    ![截屏2025-06-15 11 55
    56](https://github.com/user-attachments/assets/0db4f4f2-c401-4c34-9084-e3a26a28b28f)
    ![截屏2025-06-15 11 58
    39](https://github.com/user-attachments/assets/5a8cbd70-6f9d-4696-8347-2c02570b03d9)
    ![截屏2025-06-15 11 58
    29](https://github.com/user-attachments/assets/ef36d7a3-9442-4305-b28e-1210324b10e4)
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code
    - [ ] This PR does not introduce a breaking change
    - [ ] Unit tests are added/updated (if applicable)
    - [x] No error nor warning in the console
    - [x] I have uploaded a screenshot (if applicable)

--- src/main/java/net/ow/movie/theatre/controller/ConfigurationController.java
+++ src/main/java/net/ow/movie/theatre/controller/ConfigurationController.java
@@ -3,7 +3,9 @@ package net.ow.movie.theatre.controller;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import net.ow.movie.theatre.dto.country.CountryDTO;
+import net.ow.movie.theatre.dto.genre.GenreDTO;
 import net.ow.movie.theatre.service.CountryService;
+import net.ow.movie.theatre.service.GenreService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +17,23 @@ import org.springframework.web.bind.annotation.*;
 public class ConfigurationController {
     private final CountryService countryService;
 
+    private final GenreService genreService;
+
     @GetMapping("/countries")
     public ResponseEntity<List<CountryDTO>> getCountries(
             @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE) String language) {
         return ResponseEntity.ok(countryService.getCountries(language));
+    }
+
+    @GetMapping("/genres/movie")
+    public ResponseEntity<List<GenreDTO>> getMovieGenres(
+            @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE) String language) {
+        return ResponseEntity.ok(genreService.getMovieGenres(language));
+    }
+
+    @GetMapping("/genres/tv-show")
+    public ResponseEntity<List<GenreDTO>> getTVShowGenres(
+            @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE) String language) {
+        return ResponseEntity.ok(genreService.getTVShowGenres(language));
     }
 }
